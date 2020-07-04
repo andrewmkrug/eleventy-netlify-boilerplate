e05f6606811bb3bfe966777f1586106ab91044ac
Andrew Krug <andrew.m.krug@gmail.com>
2020-07-04 17:10:30 +0000

    init

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -7,3 +7,18 @@ if (window.netlifyIdentity) {
     }
   });
 }
+
+
+export const qs = (el, parent = document) => parent.querySelector(el)
+
+export const qsa = (el, parent = document) =>
+  Array.from(parent.querySelectorAll(el))
+
+export const on = (event, el, fn) => el.addEventListener(event, fn)
+
+export const run = (element, fn) => {
+  const matches = qsa(element)
+  if (matches.length > 0) {
+    matches.forEach(match => fn(match))
+  }
+}
